8b479222de3a1d00a9985da524e2bb7e1355be2c
ssbrewster <ssbrewster@gmail.com>
2014-11-03 10:46:11 +0000

    Imported RedirectView as class based view for redirect to anonymeyes app

--- cgrn/urls.py
+++ cgrn/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import redirect_to
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', redirect_to, {'url': 'anonymeyes'}),
+    url(r'^$', RedirectView.as_view(url='anonymeyes')),
     url(r'^anonymeyes/', include('apps.anonymeyes.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
