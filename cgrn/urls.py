933e191aed9d6fd1716a94011d5a80c869a25999
ssbrewster <ssbrewster@gmail.com>
2014-11-05 10:33:01 +0000

    Updated anonymeyes urls include

--- cgrn/urls.py
+++ cgrn/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url='anonymeyes')),
-    url(r'^anonymeyes/', include('.anonymeyes.urls')),
+    url(r'^anonymeyes/', include('cgrn.apps.anonymeyes.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^admin/', include(admin.site.urls)),
